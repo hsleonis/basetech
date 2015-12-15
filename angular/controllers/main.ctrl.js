9136780b9cb8f218d6e7b3df867fe3c4fdc0991d
Shahriar <hsleonis@gmail.com>
2015-12-15 10:05:44 +0600

    Routing templates

--- angular/controllers/main.ctrl.js
+++ angular/controllers/main.ctrl.js
@@ -1,1 +1,9 @@
 /* angular/controllers/ -> main.ctrl.js file */
+
+app.controller('mainController', function($scope) {
+    $scope.message = 'Home page';
+});
+
+app.controller('listController', function($scope) {
+    $scope.message = 'Everyone come and see how good I look!';
+});
--- angular/router/router.js
+++ angular/router/router.js
@@ -1,1 +1,12 @@
 /* angular/router/ -> router.js file, /* Router file */
+app.config(function($routeProvider,$locationProvider) {
+    $routeProvider
+    .when('/', {
+        controller  : 'mainController'
+    })
+    .when('/:page', {
+        templateUrl : 'templates/listview.html',
+        controller  : 'listController'
+    });
+    $locationProvider.html5Mode(true);
+});
